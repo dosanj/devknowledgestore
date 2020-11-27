397818bfbc7f53d51fe9c375e5e8da85b168072c
dosanj <javeddosani2011@gmail.com>
2020-11-27 19:08:23 +0530

    use custom element decorator to add components

--- src/js/components/container/container.comp.ts
+++
@@ -1,6 +0,0 @@
-import { BaseComponent } from '../base.comp';
-import htmlTemplate from './container-template.html';
-
-export class ContainerComponent extends BaseComponent {
-  template = htmlTemplate;
-}
