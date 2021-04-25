ddc88a664ade60c6602e2f2e111f1a71f524c074
dosanj <javeddosani2011@gmail.com>
2021-04-26 03:49:36 +0530

    test credential

--- api/firestore.ts
+++ api/firestore.ts
@@ -1,6 +1,7 @@
 const { Firestore } = require('@google-cloud/firestore');
 
 let firestore = null;
+console.log(process.env.GCLOUD_CREDENTIALS);
 export function getFireStoreDB() {
   if (!firestore) {
     firestore = new Firestore();
