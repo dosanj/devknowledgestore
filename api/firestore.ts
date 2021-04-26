83682820d82d251c40d2d3652912f5922c1e56fa
dosanj <javeddosani2011@gmail.com>
2021-04-26 05:32:20 +0530

    check if google auth works as expected

--- api/firestore.ts
+++ api/firestore.ts
@@ -2,16 +2,14 @@ const { Firestore } = require('@google-cloud/firestore');
 const fs = require('path');
 
 let firestore = null;
-let buff = Buffer.from(process.env.GCLOUD_CREDENTIALS, 'base64');
-let credentials = buff.toString('ascii');
-fs.writeFileSync('./credentials.json', credentials);
 export function getFireStoreDB() {
   if (!firestore) {
-    const firestore = new Firestore({
-      projectId: 'linkstore-821d8',
-      keyFilename: './credentials.json',
-    });
-    firestore.settings({
+    let buff = Buffer.from(process.env.GCLOUD_CREDENTIALS, 'base64');
+    let { client_email, private_key, project_id: projectId } = JSON.parse(buff.toString('ascii'));
+    firestore = new Firestore({
+      projectId,
+      client_email,
+      private_key,
       ignoreUndefinedProperties: true,
     });
   }
--- api/test.ts
+++
@@ -1,8 +0,0 @@
-import { VercelRequest, VercelResponse } from '@vercel/node';
-console.log(process.env.GCLOUD_CREDENTIALS);
-export default async (req: VercelRequest, res: VercelResponse) => {
-  let buff = Buffer.from(process.env.GCLOUD_CREDENTIALS, 'base64');
-  let text = buff.toString('ascii');
-  res.statusCode = 200;
-  res.json(JSON.parse(text));
-};
